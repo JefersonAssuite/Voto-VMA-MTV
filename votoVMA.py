b022ed776dd153d01001d1daadb3ad036265eb8b
Jeferson Assuite <86379148+JefersonAssuite@users.noreply.github.com>
2024-08-15 11:22:42 -0300

    Create votoVMA.py
    A automação funcionando.

---
+++ votoVMA.py
@@ -0,0 +1,45 @@
+import pyautogui
+import time
+
+pyautogui.PAUSE = 0.8
+
+#pyautogui.press('win')
+
+#pyautogui.write("chrome")
+
+#pyautogui.press('enter')
+
+#pyautogui.hotkey('ctrl', 'shift', 'n')
+
+#pyautogui.write("https://www.mtv.com/vma/vote/")
+
+#pyautogui.press('enter')
+
+#time.sleep(7)
+
+pyautogui.click(x=-550, y=684)
+
+pyautogui.scroll(-2000)
+time.sleep(3)
+
+pyautogui.click(x=-535, y=545)
+time.sleep(3)
+
+pyautogui.click(x=-684, y=427)
+
+pyautogui.scroll(-300)
+
+pyautogui.click(x=-1029, y=558,clicks=10)
+time.sleep(3)
+
+pyautogui.scroll(-1500)
+
+pyautogui.click(x=-682, y=460)
+#pyautogui.write("jefersonassuite249@gmail.com")
+
+#pyautogui.click(x=-675, y=474)
+#time.sleep(2)
+
+
+#pyautogui.scroll(-600)
+#pyautogui.click(x=-705, y=606)
